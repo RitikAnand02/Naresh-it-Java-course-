5cf79d16949081cad65407cc7797d10bb3dee606
RitikAnand02 <ritikanand.02062000@gmail.com>
2024-12-06 20:06:35 +0530

    new

---
+++ Naresh it java  hw code/Exception_Handling/src/com/test/ReturnExample.java
@@ -0,0 +1,27 @@
+package com.test;
+
+public class ReturnExample
+{
+    public static void main(String[] args) 
+    {     	
+       System.out.println(methodReturningValue());        
+    }	
+	
+	
+	public static int methodReturningValue()
+    {
+        try
+        {
+            System.out.println("Try block");
+            return 10/0;    
+        }
+        catch (Exception e)
+        {
+            System.out.println("catch block");         
+           return 20;  //return statement is compulsory  
+        }
+        
+       // System.out.println("Unreachable code");
+               
+    }
+}
